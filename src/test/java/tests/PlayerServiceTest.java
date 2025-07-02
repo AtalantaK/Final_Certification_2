2cb6b419b705a2e52c5e2462edada85b02e3a907
Xenia <aksinia.ra@gmail.com>
2025-07-02 13:42:20 +0500

    Added Test#2

--- src/test/java/tests/PlayerServiceTest.java
+++ src/test/java/tests/PlayerServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import ru.inno.course.player.model.Player;
 import ru.inno.course.player.service.PlayerService;
 import ru.inno.course.player.service.PlayerServiceImpl;
@@ -15,9 +16,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerServiceTest {
 
@@ -32,9 +33,9 @@ public class PlayerServiceTest {
     }
 
     @Test
-    @DisplayName("Добавить игрока")
+    @DisplayName("Добавить игрока в пустой список")
     @Tag("Positive_TC")
-    public void createPlayerTest() {
+    public void createPlayerTestInEmptyList() {
         PlayerService service = new PlayerServiceImpl();
         String expectedPlayerNick = "Nick";
         int expectedPlayerId = 1;
@@ -43,5 +44,66 @@ public class PlayerServiceTest {
         String expectedPlayer = "Player{id=" + expectedPlayerId + ", nick='" + expectedPlayerNick + "', points=0, isOnline=true}";
 
         assertEquals(expectedPlayer, service.getPlayerById(actualPlayerId).toString());
+    }
+
+    //todo: есть ли смысл проверять в какой список мы добавляем игрока? - возможно есть
+    @Test
+    @DisplayName("Добавить игрока в не пустой список")
+    @Tag("Positive_TC")
+    public void createPlayerTestInNotEmptyList() {
+        PlayerService service = new PlayerServiceImpl();
+        String expectedPlayerNick = "Nick2";
+        int expectedPlayerId = 2;
+
+        service.createPlayer("Nick1");
+        int actualPlayerId = service.createPlayer(expectedPlayerNick);
+
+        String expectedPlayer = "Player{id=" + expectedPlayerId + ", nick='" + expectedPlayerNick + "', points=0, isOnline=true}";
+
+        assertEquals(expectedPlayer, service.getPlayerById(actualPlayerId).toString());
+    }
+
+    @Test
+    @DisplayName("Удалить не последнего игрока")
+    @Tag("Positive_TC")
+    public void deleteNotLastPlayer() {
+        PlayerService service = new PlayerServiceImpl();
+        String expectedDeletedPlayerNick = "Nick1";
+        int expectedDeletedPlayerId = 1;
+        service.createPlayer(expectedDeletedPlayerNick);
+        service.createPlayer("Nick2");
+        Player deletedPlayer = service.deletePlayer(expectedDeletedPlayerId);
+
+        String expectedDeletedPlayer = "Player{id=" + expectedDeletedPlayerId + ", nick='" + expectedDeletedPlayerNick + "', points=0, isOnline=true}";
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            service.getPlayerById(expectedDeletedPlayerId);
+        });
+
+        assertAll("Несколько проверок",
+                () -> assertEquals(expectedDeletedPlayer, deletedPlayer.toString()),
+                () -> assertEquals("No such user: " + expectedDeletedPlayerId, exception.getMessage()));
+    }
+
+    @Test
+    @DisplayName("Удалить последнего игрока")
+    @Tag("Positive_TC")
+    public void deleteLastPlayer() {
+        PlayerService service = new PlayerServiceImpl();
+        String expectedDeletedPlayerNick = "Nick1";
+        int expectedDeletedPlayerId = 1;
+        service.createPlayer(expectedDeletedPlayerNick);
+        Player deletedPlayer = service.deletePlayer(expectedDeletedPlayerId);
+
+        String expectedDeletedPlayer = "Player{id=" + expectedDeletedPlayerId + ", nick='" + expectedDeletedPlayerNick + "', points=0, isOnline=true}";
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            service.getPlayerById(expectedDeletedPlayerId);
+        });
+
+        assertAll("Несколько проверок",
+                () -> assertEquals(expectedDeletedPlayer, deletedPlayer.toString()),
+                () -> assertEquals("No such user: " + expectedDeletedPlayerId, exception.getMessage()),
+                () -> assertTrue(service.getPlayers().isEmpty()));
     }
 }
